1b0acafacb0aa8e054632b12d4424e2ce5515984
Lukas Frenzke <98991019+LukasFrenzke@users.noreply.github.com>
2022-03-03 11:26:06 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 import Quest5.Evaluate;
-import Quest6.Classroom;
 
 public class Main {
     public static void main(String[] args) {
         Evaluate.eval();
-        //Classroom.main();
     }
 }
